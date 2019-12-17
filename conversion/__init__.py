b20d75edf296d4181f583c6c2fbeb2575c9cc2f6
maruyu998 <maruyu998@gmail.com>
2019-12-17 14:46:15 +0900

    modified conversion.init

--- conversion/__init__.py
+++ conversion/__init__.py
@@ -1,9 +1,8 @@
-from conversion import *
-from conversion.conversion_list import *
+from narouresearch.conversion.conversion_list import *
 import os
 list = create_char_range_list()
-pwd = __file__[:-11]+"convert.py" #実行ディレクトリの絶対パス取ってくる
-f = open(pwd+"convert.py", "w")
+path = __file__[:-11]+"convert.py"
+f = open(path, "w")
 f.close()
-print_if_sentence_char2ID(pwd+"convert.py",list)
-print_if_sentence_ID2char(pwd+"convert.py",list)
+print_if_sentence_char2ID(path,list)
+print_if_sentence_ID2char(path,list)
