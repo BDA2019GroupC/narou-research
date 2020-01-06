3539ea7acdc79ec7f739aa4e59c810e3ba4056fa
Yudai FUJIWARA <ptr.yudai@gmail.com>
2020-01-06 21:42:53 +0900

    Updated

--- utils/preprocess/main.py
+++ utils/preprocess/main.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 import io
+import os
 import glob
 from narocessor import Processor
 
 if __name__ == '__main__':
     for path in glob.glob('classified/*/N*.txt'):
-        print(path.replace('classified', 'processed'))
-        exit()
-        fout = open('', 'w')
+        print(path)
+        opath = path.replace('classified', 'processed')
+        dirpath = os.path.dirname(opath)
+        if not os.path.exists(dirpath):
+            os.makedirs(dirpath)
+        fout = open(opath, 'w')
         fin = open(path, 'r')
         p = Processor(fin, fout)
         p.preprocess()
         fin.close()
         fout.close()
-        exit() # only one
