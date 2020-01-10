2df3c0ee94e819f02220285e2b3038740e7c8174
maruyu998 <maruyu998@gmail.com>
2020-01-11 02:45:36 +0900

    adjust to same directory structure

--- utils/data_alignment.py
+++ utils/data_alignment.py
@@ -1,26 +1,22 @@
 from narouresearch.dataloader.dataloader import DataLoader
 import os
 import logging
+import sys
 
-def data_alignment_by_length(from_rootpath, to_rootpath):
-	os.makedirs(to_rootpath, exist_ok=True)
-	dl = DataLoader(path,validation_split=0)
-	generator = dl.get_generator()
-	for i, sentence in enumerate(generator):
-		write_sentence_by_length(to_rootpath, sentence)
+def data_alignment_by_length(source_rootpath, dest_rootpath, depth):
+    os.makedirs(dest_rootpath, exist_ok=True)
+    dl = DataLoader(source_rootpath)
+    generator = dl.get_generator()
+    for i, (path, sentence) in enumerate(generator):
+        if i%100000==0: print(i)
+        logging.info("{} sentences are processed".format(i))
+        destdir = dest_rootpath + "/".join(path.split("/")[-depth:]).split(".")[0]+"/"
+        os.makedirs(destdir, exist_ok=True)
+        write_sentence_by_length(destdir, sentence)
+        logging.debug(str(i),dest_rootpath,path,sentence)
 
 def write_sentence_by_length(rootpath,sentence):
-	length = str(len(sentence))
-	with open(rootpath+'/'+length+'.txt','a') as f:
-		f.write(sentence)
-		f.write('\n')
-
-def split_file_by_sentence_length(root, generator, start=0):
-  for j,(tpath,sentence) in enumerate(generator):
-      if j < start : continue
-      if j % 10000 == 0: 
-          logging.info("{} sentences are processed".format(j))
-
-      direc = root+'Data/narou_sentences_by_length/'+'/'.join(tpath.split('/')[-2:]).split(".")[0]
-      write_sentence_by_length(direc, sentence)
-      logging.debug(str(j),direc,sentence)
+    length = str(len(sentence))
+    with open(rootpath+length+'.txt','a') as f:
+        f.write(sentence)
+        f.write('\n')
