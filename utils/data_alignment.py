56bde22af7f2c9166821919e4937ddf01aae0060
maruyu998 <maruyu998@gmail.com>
2020-01-11 02:00:00 +0900

    add split_file_by_sentence_length func

--- utils/data_alignment.py
+++ utils/data_alignment.py
@@ -1,5 +1,6 @@
 from narouresearch.dataloader.dataloader import DataLoader
 import os
+import logging
 
 def data_alignment_by_length(from_rootpath, to_rootpath):
 	os.makedirs(to_rootpath, exist_ok=True)
@@ -13,3 +14,13 @@ def write_sentence_by_length(rootpath,sentence):
 	with open(rootpath+'/'+length+'.txt','a') as f:
 		f.write(sentence)
 		f.write('\n')
+
+def split_file_by_sentence_length(root, generator, start=0):
+  for j,(tpath,sentence) in enumerate(generator):
+      if j < start : continue
+      if j % 10000 == 0: 
+          logging.info("{} sentences are processed".format(j))
+
+      direc = root+'Data/narou_sentences_by_length/'+'/'.join(tpath.split('/')[-2:]).split(".")[0]
+      write_sentence_by_length(direc, sentence)
+      logging.debug(str(j),direc,sentence)
