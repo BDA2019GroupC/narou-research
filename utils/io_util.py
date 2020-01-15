5d963c82e653bef6d399051d6648cce76d3e2037
maruyu998 <maruyu998@gmail.com>
2020-01-15 17:18:31 +0900

    added get_limited_shuffle_path function

--- utils/io_util.py
+++ utils/io_util.py
@@ -1,6 +1,16 @@
 import os
 import random
 
+def detect_encoding(file):
+    from chardet.universaldetector import UniversalDetector
+    det = UniversalDetector()
+    with open(file,'rb') as f:
+        for bin in f:
+            det.feed(bin)
+            if det.done : break
+            det.close()
+    return det.result["encoding"]
+
 def get_all_path(rootpath, extention=[], exception=[], absolute=False, shuffle=False, seed=0, init=True):
     if init: random.seed(seed)
     rootpath = rootpath.rstrip('/')
@@ -19,16 +29,6 @@ def get_all_path(rootpath, extention=[], exception=[], absolute=False, shuffle=F
             if len(extention) == 0 or ext in extention:
                 yield joinedpath
 
-def detect_encoding(file):
-    from chardet.universaldetector import UniversalDetector
-    det = UniversalDetector()
-    with open(file,'rb') as f:
-        for bin in f:
-            det.feed(bin)
-            if det.done : break
-            det.close()
-    return det.result["encoding"]
-
 def get_path_by_length(rootpath, length, extention=[], exception=[], absolute=False, shuffle=False, seed=0, init=True):
     if init: random.seed(seed)
     rootpath = rootpath.rstrip('/')
@@ -43,6 +43,24 @@ def get_path_by_length(rootpath, length, extention=[], exception=[], absolute=Fa
             for path in get_path_by_length(joinedpath, length, extention, exception, absolute, shuffle, init=False):
                 if int(path.split("/")[-1].split('.')[0]) == length:
                     yield path
+        else:
+            ext = file.split('.')[-1]
+            if len(extention) == 0 or ext in extention:
+                yield joinedpath
+
+def get_limited_shuffle_path(rootpath, limit_rate=0.2 extention=[], exception=[], absolute=False, seed=0, init=True):
+    if init: random.seed(seed)
+    rootpath = rootpath.rstrip('/')
+    files = os.listdir(rootpath)
+    random.shuffle(files)
+    for file in files:
+        if file in exception: continue
+        if random.random() >= limit_rate: continue
+        
+        joinedpath = os.path.join(rootpath, file)
+        if os.path.isdir(joinedpath):
+            for path in get_limited_shuffle_path(joinedpath, limit_rate, extention, exception, absolute, init=False):
+                yield path
         else:
             ext = file.split('.')[-1]
             if len(extention) == 0 or ext in extention:
