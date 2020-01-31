1c805057b2038b37dd0267a0af05ec24b6c2d684
maruyu998 <maruyu998@gmail.com>
2020-01-31 15:16:19 +0900

    add bert

---
+++ networks/BERT/model.py
@@ -0,0 +1,5 @@
+import os, time, random, gc
+import numpy as np
+import torch
+import torch.optim as optim
+
---
+++ networks/BERT/train.py
@@ -0,0 +1,4 @@
+import os, time, random, gc
+import numpy as np
+import torch
+import torch.optim as optim
